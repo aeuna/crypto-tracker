ad8bb69857f276d10e7a6f96c90a8a114cca8a0c
aeuna <4294psy@naver.com>
2022-01-26 22:06:33 +0900

    5. fetch coin data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -45,8 +45,6 @@ function Coin() {
       const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
       setInfo(infoData);
       setPriceInfo(priceData);
-      console.log(infoData);
-      console.log(priceData);
     })();
   }, []);
   return (
